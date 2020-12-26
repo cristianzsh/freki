42364ec4ceffccf95dc78240febd5628c93fd271
crhenr <cristianmsbr@gmail.com>
2020-12-25 22:38:49 -0300

    Improve dockerfiles structure

---
+++ docker/mariadb/startup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ -d "/run/mysqld" ]; then
+    echo "[*] mysqld already present"
+    chown -R mysql:mysql /run/mysqld
+else
+    echo "[*] mysqld not found, creating..."
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+if [ -d /var/lib/mysql/mysql ]; then
+    chown -R mysql:mysql /var/lib/mysql
+else
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+
+    if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+        MYSQL_ROOT_PASSWORD=`pwgen 24 1`
+        echo "[*] MySQL root password: $MYSQL_ROOT_PASSWORD"
+    fi
+
+    MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+    MYSQL_USER=${MYSQL_USER:-""}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');
+DROP DATABASE test;
+EOF
+
+    if [ "$MYSQL_DATABASE" != "" ]; then
+        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+        if [ "$MYSQL_USER" != "" ]; then
+            echo "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+            echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+        fi
+    fi
+
+    echo "FLUSH PRIVILEGES;" >> $tfile
+    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+    rm -f $tfile
+fi
+
+sleep 5
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
